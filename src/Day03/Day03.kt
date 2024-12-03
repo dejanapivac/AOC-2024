46d7d14f718a8398a32079aee264074290406859
Dejana Pivac <dejanapivac@gmail.com>
2024-12-03 16:38:39 +0100

    Day 03 first working version

--- src/Day03/Day03.kt
+++ src/Day03/Day03.kt
@@ -1,16 +1,34 @@
 package Day03
 
 import println
-import readInput
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+fun findFunction(numbers: String): Sequence<MatchResult> {
+    val pattern = Regex("""mul\((\d{1,3},\d{1,3})\)""")
+    return pattern.findAll(numbers)
+}
 
 
 fun main() {
-    fun part1(input: List<String>): Long {
-        return 161L
+    fun part1(input: String): Long {
+        val matches = findFunction(input)
+        val sumOfAll = matches.map { match ->
+            val numbers = match.groupValues[1]
+                .split(",")
+                .map { it.toLong() }
+            numbers[0] * numbers[1]
+        }.sum()
+
+        return sumOfAll
     }
 
     fun part2(input: List<String>): Long {
         return 0L
+    }
+
+    fun readInput(name: String, folder: String): String {
+        return Path("src/$folder/$name.txt").readText().trim()
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
